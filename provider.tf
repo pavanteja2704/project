a68ff656bbbf38db9b3f9196ce297c003d9acd81
pavan2704 <138353777+pavanteja2704@users.noreply.github.com>
2024-06-12 10:42:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   region = "us-central1"
   project = "hardy-binder-411706"
-  credentials = /workspaces/jenkins/hardy-binder-411706-334012125403.json
+  credentials = hardy-binder-411706-334012125403.json
 }
  
